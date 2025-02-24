23ffa34a47b939d37120f626d92f9caf52e09955
Diana-Pavlovska <dianapavlovskayaya@gmail.com>
2025-02-24 23:47:37 +0200

    Created homework 3.1

---
+++ Homework 3.1.py
@@ -0,0 +1,17 @@
+number1=float(input("Enter first number:"))
+operator=input("Enter(+,-,*,/):")
+number2=float(input("Enter second number:"))
+if operator =="+":
+    result=number1+number2
+elif operator =="-":
+    result = number1-number2
+elif operator =="*":
+    result = number1*number2
+elif operator =="/":
+    if number2==0:
+        result="Error!"
+    else:
+     result = number1/number2
+else:
+    result="Unknown!"
+print("Result:", result)
