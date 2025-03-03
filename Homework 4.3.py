57fb8ec4443ec59a9b073185cc95e2693c44e26e
Diana-Pavlovska <dianapavlovskayaya@gmail.com>
2025-03-03 19:36:21 +0200

    Created homework 4.3 (add random lists)

--- Homework 4.3.py
+++ Homework 4.3.py
@@ -5,6 +5,11 @@ examples = [
     [6, 3, 7]
 ]
 for example in examples:
-    new_list = [example[0], example[2], example[-2]]
+    new_list1 = [example[0], example[2], example[-2]]
     print("Input:", example)
-    print("Output:", new_list)
+    print("Output:", new_list1)
+#add random lists
+random_list = [random.randint(1, 100) for option in range(random.randint(3, 10))]
+new_list2 = [random_list[0], random_list[2], random_list[-2]]
+print("Random:", random_list)
+print("Result:", new_list2)
