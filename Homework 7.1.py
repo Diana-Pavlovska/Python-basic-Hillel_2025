cf59e664b2f694f1fa9958e5799349875d61fd4a
Diana-Pavlovska <dianapavlovskayaya@gmail.com>
2025-04-11 22:29:37 +0300

    Created homework 7.1

---
+++ Homework 7.1.py
@@ -0,0 +1,6 @@
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old"
+assert say_hi("Diana", 27) == "Hi. My name is Diana and I'm 27 years old", 'Test1'
+print('OK')
+
+
